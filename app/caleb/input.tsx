3bf7f7465f8f9f42c17e265098fd327045388e57
James418-alt <ebifegha123@gmail.com>
2024-07-28 23:54:47 +0100

    work tree fixed

--- app/caleb/input.tsx
+++ app/caleb/input.tsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
- 
 export function InputWithButton() {
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="email" placeholder="Email or mobile number" />
-      <Button type="submit">Get Started</Button>
+      <input type="email" placeholder="Email or mobile number" />
+      <button type="submit">Get Started</button>
     </div>
-  )
+  );
 }
-export default InputWithButton
+export default InputWithButton;
